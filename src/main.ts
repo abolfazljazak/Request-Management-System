953dab465d217c118d88cc889633d61ee4bf6849
abolfazl jazak <jazakabolfazl@gmail.com>
2025-03-26 02:44:32 +0330

    chore(config): install dotenv and configure environment variables
    - Installed `dotenv` package to manage environment variables.
    - Added `dotenv.config()` to `main.ts` for loading environment variables from `.env` file.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
