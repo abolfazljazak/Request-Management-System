745b84385b4bb2b3235aa0c9e655b0fcee65ff7c
abolfazl jazak <jazakabolfazl@gmail.com>
2025-03-26 16:32:20 +0330

    feat(infrastructure): add zippo and encryption services integration
    - Integrate `ZippoService` for fetching city data by postcode.
    - Add `EncryptionService` for password encryption and decryption.
    - Extend `InfrastructureService` with methods for city data retrieval and password handling.

--- src/modules/Infrastructure/infrastructure.service.ts
+++ src/modules/Infrastructure/infrastructure.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import { TokensPayload } from "./types/palyload";
+import { PostCodeDto } from "./dtos/postcode.dto";
+import { EncryptionService } from "./services/encryption.service";
+import { ZippoResponse } from "./interfaces/zippo.interface";
+import { ZippoService } from "./services/zippo.service";
 
 @Injectable()
 export class InfrastructureService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
+    private readonly encryptionService: EncryptionService,
+    private readonly zippoService: ZippoService,
   ) {}
 
   async createAccessToken(payload: TokensPayload) {
@@ -17,5 +23,17 @@ export class InfrastructureService {
     });
 
     return accessToken;
+  }
+
+  async getCityData(PostCodeDto: PostCodeDto): Promise<ZippoResponse> {
+    return this.zippoService.getCityData(PostCodeDto);
+  }
+
+  encryptPassword(password: string): string {
+    return this.encryptionService.encrypt(password);
+  }
+
+  decryptPassword(password: string): string {
+    return this.encryptionService.decrypt(password);
   }
 }
