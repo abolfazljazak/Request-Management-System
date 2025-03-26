6d088e5bec7d033550124dd71d94fd17fc3fdb39
abolfazl jazak <jazakabolfazl@gmail.com>
2025-03-26 14:49:23 +0330

    feat(infrastructure): add axios and @nestjs/axios to Infrastructure module
    - Installed `axios` and `@nestjs/axios` packages.
    - Added `HttpModule` to `InfrastructureModule`.
    - Updated `package.json` and `package-lock.json` with the new dependencies.

--- src/modules/Infrastructure/infrastructure.module.ts
+++ src/modules/Infrastructure/infrastructure.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import { InfrastructureService } from "./infrastructure.service";
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [InfrastructureService, JwtService, ConfigService],
   exports: [InfrastructureService, JwtService, ConfigService],
 })
